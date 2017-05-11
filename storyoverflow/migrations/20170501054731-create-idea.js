7740105548b724a6955e1f4daf18b151be231825
jamessraun <jeezybig@gmail.com>
2017-05-11 11:04:44 +0700

    first commit

---
+++ storyoverflow/migrations/20170501054731-create-idea.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Ideas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idea: {
+        type: Sequelize.STRING
+      },
+      is_accepted: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Ideas');
+  }
+};
---
+++ storyoverflow/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser =  require('body-parser');
+var path = require('path');
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
+
+//oatuh instanciate
+
+
+app.use('/',index);
+app.use('/users', users);
+
+app.listen(3000);
