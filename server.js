979c873c195972c50fb4d7f5554c20a8309e6646
stedyyulius <stedyyulius@gmail.com>
2017-05-19 12:04:27 +0700

    cape

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const index = require('./routes/index');
 const user = require('./routes/user');
 const idea = require('./routes/idea');
@@ -6,7 +7,10 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 var app = express();
 
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'));
